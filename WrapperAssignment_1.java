14a531088667417e1845cac574a2cf3dd43e3002
Praveen-baghel <82326313+Praveen-baghel@users.noreply.github.com>
2021-11-09 16:50:43 +0530

    Add files via upload

---
+++ WrapperAssignment_1.java
@@ -0,0 +1,29 @@
+public class WrapperAssignment_1 {
+    public static void main(String[] args) {
+        Byte b1=Byte.MAX_VALUE;
+        Byte b2=Byte.MIN_VALUE;
+        Short s1=Short.MAX_VALUE;
+        Short s2=Short.MIN_VALUE;
+        Integer i1=Integer.MAX_VALUE;
+        Integer i2=Integer.MIN_VALUE;
+        Long l1=Long.MAX_VALUE;
+        Long l2=Long.MIN_VALUE;
+        Float f1=Float.MAX_VALUE;
+        Float f2=Float.MIN_VALUE;
+        Double d1=Double.MAX_VALUE;
+        Double d2=Double.MIN_VALUE;
+        System.out.println("Max. Byte: "+b1);
+        System.out.println("Min. Byte: "+b2);
+        System.out.println("Max. Short: "+s1);
+        System.out.println("Min. Short: "+s2);
+        System.out.println("Max. Integer: "+i1);
+        System.out.println("Min. Integer: "+i2);
+        System.out.println("Max. Long: "+l1);
+        System.out.println("Min. Long: "+l2);
+        System.out.println("Max. Float: "+f1);
+        System.out.println("Min. Float: "+f2);
+        System.out.println("Max. Double: "+d1);
+        System.out.println("Min. Double: "+d2);
+
+    }
+}
